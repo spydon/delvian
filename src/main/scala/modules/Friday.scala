4026b61121c5f09e9dc71524d9f699d43ed1c91b
Lukas Klingsbo <lukas.klingsbo@gmail.com>
2016-04-13 18:58:00 +0200

    Added not detection

--- src/main/scala/modules/Friday.scala
+++ src/main/scala/modules/Friday.scala
@@ -8,13 +8,13 @@ import org.joda.time.DateTime
 class Friday(override val bus: MessageEventBus) extends AbstractBot {
   val r = scala.util.Random
   val fridayMessages =
-    List("IT IS!!!", "Trevlig helg", "modules.Friday, it is!", "Have a great weekend", "See you on monday",
-    ":beers:", ":rebeccablack:")
+    List("IT IS!!!", "Trevlig helg :partyparrot:", "Friday, it is!", "Have a great weekend", "See you on monday!",
+         ":beers:", ":rebeccablack:", ":partyparrot:", ":party:")
 
   val sadMessages =
     List("Trevlig vardag", "Unfortunately not", "Doesn't matter...", "Still a bit to go", "NEIN", "Check the calendar",
          "You work in a great place, stop asking!", "How come your existence revolve around a day that is not today?",
-         "Nope.", ":'(", ":lipssealed:")
+         "Nope.", ":sadpanda:", ":lipssealed:")
 
   val triggerWords = List("friday", "fredag", "vrijdag", "reede", "perjantai", "freitag", "föstudagur", "jumat",
                           "venerdì", "piątek", "vineri", "viernes")
@@ -25,7 +25,7 @@ class Friday(override val bus: MessageEventBus) extends AbstractBot {
       "Usage: call on the bot and include friday in the message")
 
   def trigger(msg: String): Boolean = {
-    val stripped = msg.replace("?", "").replace("!", "").replace(".", "")
+    val stripped = msg.replace("?", "").replace("!", "").replace(".", "").replace(",", "")
     triggerWords.contains(stripped.toLowerCase)
   }
 
@@ -38,9 +38,21 @@ class Friday(override val bus: MessageEventBus) extends AbstractBot {
     else           sadMessages(r.nextInt(sadMessages.length))
   }
 
+  // If a user tries to trick the bot with not
+  def notMessage(notNumber: Int): String = {
+    var not = ""
+    for(_ <- 1 to notNumber) not = not.concat("not ")
+
+    if(notNumber % 2 == 0) resultMessage()
+    else if(isFriday) "It is " + not + "not friday! " + resultMessage()
+    else "It is " + not + "friday... " + resultMessage()
+  }
+
   override def act: Receive = {
     case Command(command, args, message) if (command :: args).foldLeft(false)((acc, c) => acc || trigger(c)) =>
-      val response = OutboundMessage(message.channel, s"$resultMessage")
+      val notNumber = (command :: args).foldLeft(0)((acc, c) => acc + (if(c.toLowerCase == "not") 1 else 0))
+      val result = if(notNumber > 0) notMessage(notNumber) else resultMessage()
+      val response = OutboundMessage(message.channel, s"$result")
 
       publish(response)
   }
